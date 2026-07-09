433ac4a9e60f6b2cdb99e77afdaf039a374780ce
Javier <Javier@gmail.com>
2026-07-09 15:37:48 +0200

    message error translation added, conf/package/translation default_local fr

--- vitam_in/src/Controller/SecurityController.php
+++ vitam_in/src/Controller/SecurityController.php
@@ -14,6 +14,7 @@ class SecurityController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        $eror = dump($error);
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -21,6 +22,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'eror' => $eror,
         ]);
     }
 
