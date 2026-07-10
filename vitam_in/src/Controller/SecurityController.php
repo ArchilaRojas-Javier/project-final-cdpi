752ed35ab4fb3edc358382e28bbae7542760e79e
Javier <Javier@gmail.com>
2026-07-10 15:55:56 +0200

    redirection from login to dashboard added

--- vitam_in/src/Controller/SecurityController.php
+++ vitam_in/src/Controller/SecurityController.php
@@ -14,10 +14,12 @@ class SecurityController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        
-
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
