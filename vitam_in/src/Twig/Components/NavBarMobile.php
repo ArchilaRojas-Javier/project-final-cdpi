8b97dbb03018bbcbe4e4035dabf7dbae11617eb0
Javier <Javier@gmail.com>
2026-07-28 14:48:32 +0200

    We hide the navbar on the home page when the user is not logged in

--- vitam_in/src/Twig/Components/NavBarMobile.php
+++ vitam_in/src/Twig/Components/NavBarMobile.php
@@ -2,6 +2,8 @@
 
 namespace App\Twig\Components;
 use App\Service\MenuService;
+use Symfony\Bundle\SecurityBundle\Security;
+
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
@@ -10,7 +12,20 @@ final class NavBarMobile
 {
     public function __construct(
         private MenuService $menuService,
+        private Security $security,
+
     ) {
+    }
+
+     public function getShowNavBar(): bool
+    {
+        // Mostrar título si el usuario está autenticado
+        if ($this->security->getUser()) {
+            return true;
+        }
+        
+        // En cualquier otro caso (ej: inicio) no mostrar título
+        return false;
     }
 
     public function getMenuItems(): array
