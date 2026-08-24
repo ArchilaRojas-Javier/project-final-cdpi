8181f2af2f7aaf81c5d69abc41a6794c1b436bd2
Javier <Javier@gmail.com>
2026-08-24 13:46:57 +0200

    Routes added for editing and deleting a user supplement.

--- vitam_in/src/Controller/UserSupplementController.php
+++ vitam_in/src/Controller/UserSupplementController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\UserSupplement;
 use App\Form\UserSupplementType;
+use App\Service\UserSupplementService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use App\Service\UserSupplementService;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user/supplement')]
 #[IsGranted('ROLE_USER')]
 final class UserSupplementController extends AbstractController
 {
-   #[Route('/new/{supplementId}', name: 'app_user_supplement_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserSupplementService $userSupplementService, int $supplementId): Response {
-   
+    #[Route('/new/{supplementId}', name: 'app_user_supplement_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserSupplementService $userSupplementService, int $supplementId): Response
+    {
         $user = $this->getUser();
         if (!$user) {
             throw new AccessDeniedException('Vous devez être connecté pour effectuer cette action.');
@@ -38,11 +40,9 @@ final class UserSupplementController extends AbstractController
 
         $form = $this->createForm(UserSupplementType::class, $userSupplement);
         $form->handleRequest($request);
- 
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-        $userSupplementService->persistWithUser($userSupplement, $user);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userSupplementService->persistWithUser($userSupplement, $user);
             $this->addFlash('success', 'Supplément enregistré avec succès.');
             return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
         }
@@ -53,40 +53,45 @@ final class UserSupplementController extends AbstractController
         ]);
     }
 
-    // #[Route('/{id}', name: 'app_user_supplement_show', methods: ['GET'])]
-    // public function show(UserSupplement $userSupplement): Response
-    // {
-    //     return $this->render('user_supplement/show.html.twig', [
-    //         'user_supplement' => $userSupplement,
-    //     ]);
-    // }
+    #[Route('/{id}/edit', name: 'app_user_supplement_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserSupplement $userSupplement, EntityManagerInterface $entityManager): Response
+    {
+        // Vérifier que l'utilisateur connecté est bien le propriétaire
+        if ($userSupplement->getUser() !== $this->getUser()) {
+            throw new AccessDeniedException('Vous n\'avez pas le droit de modifier ce supplément.');
+        }
 
-    // #[Route('/{id}/edit', name: 'app_user_supplement_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, UserSupplement $userSupplement, EntityManagerInterface $entityManager): Response
-    // {
-    //     $form = $this->createForm(UserSupplementType::class, $userSupplement);
-    //     $form->handleRequest($request);
+        $form = $this->createForm(UserSupplementType::class, $userSupplement);
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Supplément modifié avec succès.');
+            return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
+        }
 
-    //         return $this->redirectToRoute('app_user_supplement_index', [], Response::HTTP_SEE_OTHER);
-    //     }
+        return $this->render('user_supplement/edit.html.twig', [
+            'user_supplement' => $userSupplement,
+            'form' => $form,
+        ]);
+    }
 
-    //     return $this->render('user_supplement/edit.html.twig', [
-    //         'user_supplement' => $userSupplement,
-    //         'form' => $form,
-    //     ]);
-    // }
+    #[Route('/{id}', name: 'app_user_supplement_delete', methods: ['POST'])]
+    public function delete(Request $request, UserSupplement $userSupplement, EntityManagerInterface $entityManager): Response
+    {
+        // Vérifier que l'utilisateur connecté est bien le propriétaire
+        if ($userSupplement->getUser() !== $this->getUser()) {
+            throw new AccessDeniedException('Vous n\'avez pas le droit de supprimer ce supplément.');
+        }
 
-    // #[Route('/{id}', name: 'app_user_supplement_delete', methods: ['POST'])]
-    // public function delete(Request $request, UserSupplement $userSupplement, EntityManagerInterface $entityManager): Response
-    // {
-    //     if ($this->isCsrfTokenValid('delete'.$userSupplement->getId(), $request->getPayload()->getString('_token'))) {
-    //         $entityManager->remove($userSupplement);
-    //         $entityManager->flush();
-    //     }
+        if ($this->isCsrfTokenValid('delete' . $userSupplement->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($userSupplement);
+            $entityManager->flush();
+            $this->addFlash('success', 'Supplément supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide.');
+        }
 
-    //     return $this->redirectToRoute('app_user_supplement_index', [], Response::HTTP_SEE_OTHER);
-    // }
+        return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
+    }
 }
--- vitam_in/src/Twig/Components/DashboardSupplementList.php
+++ vitam_in/src/Twig/Components/DashboardSupplementList.php
@@ -5,236 +5,79 @@ namespace App\Twig\Components;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use App\Entity\UserSupplement;
-use App\Form\UserSupplementType;
 use App\Repository\UserSupplementRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
-use Symfony\UX\LiveComponent\ComponentWithFormTrait;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-
-
-// [AsLiveComponent('dashboard_supplement_list')]
 
 #[AsLiveComponent]
 final class DashboardSupplementList
 {
     use DefaultActionTrait;
-    use ComponentWithFormTrait;   // Para manejar formularios de edición
-
-    #[LiveProp]                   // Propiedad reactiva: al cambiarla se re-renderiza
-    public ?int $editingId = null;
 
     #[LiveProp]
     public array $userSupplements = [];
 
     private Security $security;
     private UserSupplementRepository $userSupplementRepository;
-    private EntityManagerInterface $entityManager;
-    private RouterInterface $router;
-    private FormFactoryInterface $formFactory;
 
     public function __construct(
         Security $security,
-        UserSupplementRepository $userSupplementRepository,
-        EntityManagerInterface $entityManager,
-        RouterInterface $router,
-        FormFactoryInterface $formFactory 
-        
+        UserSupplementRepository $userSupplementRepository
     ) {
         $this->security = $security;
         $this->userSupplementRepository = $userSupplementRepository;
-        $this->entityManager = $entityManager;
-        $this->router = $router;
-        $this->formFactory = $formFactory;
-        
     }
 
     /**
      * Ceci s'exécute lorsque le composant est monté
      */
     public function mount(): void
-{
-    $user = $this->security->getUser();
-    if (!$user) {
-        throw new AccessDeniedException('Vous devez vous connecter.');
-    }
-
-    /** 
-     * récupère toutes les données à utiliser dans le template dans un tableau
-     * @var UserSupplement[] $userSupplementData
-     *  */
-    $userSupplementData = $this->userSupplementRepository->findByUser($user);
-
-    $this->userSupplements = array_map(function (UserSupplement $us) {
-        $supplementId = $us->getSupplement()->getId();
-        $supplementName = $us->getSupplement()->getName();
-        $startDate = $us->getStartDate();
-        $duration = $us->getDurationDays();
-        $daysRemaining = 0;
-        if ($startDate && $duration) {
-            $end = (clone $startDate)->modify("+{$duration} days");
-            $today = new \DateTimeImmutable();
-            if ($end >= $today) {
-                $daysRemaining = $today->diff($end)->days;
-            }
+    {
+        $user = $this->security->getUser();
+        if (!$user) {
+            throw new AccessDeniedException('Vous devez vous connecter.');
         }
 
-        $schedule = $us->getDosageSchedule();
+        /** 
+         * récupère toutes les données à utiliser dans le template dans un tableau
+         * @var UserSupplement[] $userSupplementData
+         */
+        $userSupplementData = $this->userSupplementRepository->findByUser($user);
+
+        $this->userSupplements = array_map(function (UserSupplement $us) {
+            $supplementId = $us->getSupplement()->getId();
+            $supplementName = $us->getSupplement()->getName();
+            $startDate = $us->getStartDate();
+            $duration = $us->getDurationDays();
+            $daysRemaining = 0;
+            if ($startDate && $duration) {
+                $end = (clone $startDate)->modify("+{$duration} days");
+                $today = new \DateTimeImmutable();
+                if ($end >= $today) {
+                    $daysRemaining = $today->diff($end)->days;
+                }
+            }
+
+            $schedule = $us->getDosageSchedule();
             if (is_array($schedule) && isset($schedule['dose'], $schedule['unit'])) {
                 $dose = $schedule['dose'];
                 $unit = $schedule['unit'];
                 $dosageSummary = $dose . ' ' . $unit;
             } else {
-                $dosageSummary = '0'; // Valeur par défaut si aucune information sur la dose n'est disponible
+                $dosageSummary = '0';
             }
         
-        return [
-            'id'               => $us->getId(),//id de userSuplement
-            'supplementName'   => $supplementName,
-            'startDate'        => $startDate->format('Y-m-d'),
-            'durationDays'     => $duration,
-            'daysRemaining'    => $daysRemaining,
-            'dosageSummary'    => $dosageSummary,
-            'notesCount'       => $us->getNotes()->count(),
-            'supplementId'     => $supplementId, // id de Supplement
-        ];
-    }, $userSupplementData);
-}
-
-    // Construye el formulario de edición (usado solo cuando editingId no es null)
-    protected function instantiateForm(): FormInterface
-{
-    $userSupplement = null;
-    if ($this->editingId) {
-        $userSupplement = $this->userSupplementRepository->find($this->editingId);
-        if (!$userSupplement || $userSupplement->getUser() !== $this->security->getUser()) {
-            throw new AccessDeniedException();
-        }
-    } else {
-        // Para el estado inicial, creamos un objeto vacío (no se usará)
-        $userSupplement = new UserSupplement();
-        $userSupplement->setUser($this->security->getUser());
+            return [
+                'id'               => $us->getId(),
+                'supplementName'   => $supplementName,
+                'startDate'        => $startDate->format('Y-m-d'),
+                'durationDays'     => $duration,
+                'daysRemaining'    => $daysRemaining,
+                'dosageSummary'    => $dosageSummary,
+                'notesCount'       => $us->getNotes()->count(),
+                'supplementId'     => $supplementId,
+            ];
+        }, $userSupplementData);
     }
-    return $this->formFactory->create(UserSupplementType::class, $userSupplement);
-}
-    // Acción al hacer clic en "Editar"
-    #[LiveAction]
-    public function startEdit(int $id): void
-    {
-        dump('startEdit called with id: ' . $id);
-        $this->editingId = $id;
-    }
-
-    // Acción al cancelar edición
-    #[LiveAction]
-    public function cancelEdit(): void
-    {
-        $this->editingId = null;
-    }
-
-    // Acción al guardar el formulario de edición
-    #[LiveAction]
-    public function saveEdit(): void
-    {
-        // Procesa el formulario con los datos enviados
-        $this->submitForm();
-        $form = $this->getForm();
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-        /** @var UserSupplement $userSupplement */
-            $userSupplement = $form->getData();
-            
-            // Asegurar que el usuario sea el mismo (por si acaso)
-            $userSupplement->setUser($this->security->getUser());
-
-            // Convertir dosage_schedule a array si viene como string
-            // $dosage = $userSupplement->getDosageSchedule();
-            // if (is_string($dosage)) {
-            //     // Si es un string, lo convertimos a array (por ejemplo, líneas)
-            //     $lines = explode("\n", $dosage);
-            //     $userSupplement->setDosageSchedule(array_filter($lines, fn($l) => trim($l) !== ''));
-            // }
-            
-            $this->entityManager->persist($userSupplement);
-            $this->entityManager->flush();
-
-            $this->editingId = null;
-            
-            $this->mount(); // Refresca la lista
-        }
-        // Si hay errores, el componente se re-renderiza y los muestra
-    }
-
-    // Acción para borrar un suplemento
-    #[LiveAction]
-    public function delete(int $id): void
-        {
-        $user = $this->security->getUser();
-        $userSupplement = $this->userSupplementRepository->find($id);
-
-        if (!$userSupplement || $userSupplement->getUser() !== $user) {
-            throw new AccessDeniedException();
-        }
-
-        $this->entityManager->remove($userSupplement);
-        $this->entityManager->flush();
-
-        if ($this->editingId === $id) {
-            $this->editingId = null;
-        }
-        $this->mount();
-    }
-
-    #[LiveAction]
-public function test(): void
-{
-    dump('Test action triggered');
-    $this->editingId = 999;
-}
-
-    // Métodos auxiliares para el template
-        public function getDaysRemaining(UserSupplement $userSupplement): int
-    {
-                $start = $userSupplement->getStartDate();
-        $duration = $userSupplement->getDurationDays();
-        if (!$start || !$duration) {
-            return 0;
-        }
-        $end = $start->modify("+{$duration} days");
-        $today = new \DateTimeImmutable();
-        if ($end < $today) {
-            return 0;
-        }
-        return $today->diff($end)->days;
-    }
-
-    // public function getNoteCount(UserSupplement $userSupplement): int
-    // {
-    //     return $userSupplement->getNotes()->count(); // Asumiendo que tienes OneToMany
-    // }
-
-    // public function getDosageSummary(UserSupplement $userSupplement): string
-    // {
-    //     $schedule = $userSupplement->getDosageSchedule();
-    //     if (empty($schedule)) {
-    //         return 'No definida';
-    //     }
-    //     if (is_array($schedule)) {
-    //         // Si es un array, lo mostramos como lista separada por comas
-    //         return implode(', ', $schedule);
-    //     }
-    //     return (string) $schedule;
-    // }
-
-    // // Método para obtener el nombre desde la entidad relacionada Supplement
-    // public function getSupplementName(UserSupplement $userSupplement): string
-    // {
-    //     return $userSupplement->getSupplement() ? $userSupplement->getSupplement()->getName() : 'Sin nombre';
-    // }
 }
