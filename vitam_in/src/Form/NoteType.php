27caee8e34279b6c48b515c33d0784dbd16aec63
Javier <Javier@gmail.com>
2026-08-27 16:00:38 +0200

    CRUD of notes created

---
+++ vitam_in/src/Form/NoteType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Note;
+use App\Entity\UserSupplement;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('userSupplement', EntityType::class, [
+                'class' => UserSupplement::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Note::class,
+        ]);
+    }
+}
--- vitam_in/src/Repository/NoteRepository.php
+++ vitam_in/src/Repository/NoteRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Note;
+use App\Entity\UserSupplement;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +17,20 @@ class NoteRepository extends ServiceEntityRepository
         parent::__construct($registry, Note::class);
     }
 
-    //    /**
-    //     * @return Note[] Returns an array of Note objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('n')
-    //            ->andWhere('n.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('n.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    
 
-    //    public function findOneBySomeField($value): ?Note
-    //    {
-    //        return $this->createQueryBuilder('n')
-    //            ->andWhere('n.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * @return Note[] Returns an array of Note objects
+     */
+    public function findByUserSupplement(UserSupplement $userSupplement): array
+    {
+        return $this->createQueryBuilder('n')
+            ->andWhere('n.userSupplement = :userSupplement')
+            ->setParameter('userSupplement', $userSupplement)
+            ->orderBy('n.created_at', 'DESC')  
+            ->getQuery()
+            ->getResult();
+    }
+
+    
 }
