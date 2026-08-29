77fc38d8f677c8243a90a23a0c64794bd767265a
Javier <Javier@gmail.com>
2026-08-29 21:13:28 +0200

    options to delete comment added

--- vitam_in/src/Controller/CommentController.php
+++ vitam_in/src/Controller/CommentController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 #[Route('/comment')]
 final class CommentController extends AbstractController
 {
@@ -32,8 +31,7 @@ final class CommentController extends AbstractController
         if (!$supplement) {
             throw $this->createNotFoundException('Supplément introuvable');
         }
-
-        // Verificar que el usuario esté logueado (opcional)
+        // Verify that the user is logged in.
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $comment = new Comment();
@@ -78,32 +76,40 @@ final class CommentController extends AbstractController
     //     ]);
     // }
 
-    // #[Route('/{id}/edit', name: 'app_comment_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
-    // {
-    //     $form = $this->createForm(CommentType::class, $comment);
-    //     $form->handleRequest($request);
+    #[Route('/{id}/edit', name: 'app_comment_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
 
-    //         return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
-    //     }
+            return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
+        }
 
-    //     return $this->render('comment/edit.html.twig', [
-    //         'comment' => $comment,
-    //         'form' => $form,
-    //     ]);
-    // }
+        return $this->render('comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form,
+        ]);
+    }
 
-    // #[Route('/{id}', name: 'app_comment_delete', methods: ['POST'])]
-    // public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
-    // {
-    //     if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->getPayload()->getString('_token'))) {
-    //         $entityManager->remove($comment);
-    //         $entityManager->flush();
-    //     }
+    #[Route('/{id}', name: 'app_comment_delete', methods: ['POST'])]
+    public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    {
+        $supplement = $comment->getSupplement();
+         if ($comment->getUser() !== $this->getUser()) {
+            return $this->render('supplement/show.html.twig', [
+            'supplement' => $supplement,
+            ]);
+        }
+        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($comment);
+            $entityManager->flush();
+        }
 
-    //     return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
-    // }
+        return $this->render('supplement/show.html.twig', [
+            'supplement' => $supplement,
+        ]);
+    }
 }
